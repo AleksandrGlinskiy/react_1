1b632300aaa7be5d7fe0576457620c7070ff9b27
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-07-11 14:04:51 +0300

    components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Card } from "./components/Card";
+import "./index.css";
+
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Card />);
+
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode></React.StrictMode>); */
